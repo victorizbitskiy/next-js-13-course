d8e94420596aff8cd27733a0f1b0e6e19e20f651
victorizbitskiy <victor.izbitskiy@yandex.ru>
2023-03-26 21:08:20 +0300

    work with movie api

--- app/page.js
+++ app/page.js
@@ -1,7 +1,34 @@
+const axios = require("axios");
+let res = '';
+
 export default function Home() {
+
+  const options = {
+    method: 'GET',
+    url: 'https://online-movie-database.p.rapidapi.com/auto-complete',
+    params: { q: 'western' },
+    headers: {
+      'X-RapidAPI-Key': `${process.env.API_KEY}`,
+      'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
+    }
+  };
+
+  axios.request(options).then(function (response) {
+    res = response.data.d
+  }).catch(function (error) {
+    console.error(error)
+  });
+
+  console.log(res);
+
   return (
     <main >
       <h1 >Hello Next 13!</h1>
+      {res.map((movie => (
+        <div>
+          <h1>{movie.l}</h1>
+        </div>
+      )))}
     </main>
   )
 }
